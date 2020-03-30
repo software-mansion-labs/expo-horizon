justingwl/expo

[expo-notifications] Unify types across the API

packages/expo-notifications/build/NotificationScheduler.d.ts
@@ -1,11 +1,9 @@
 import { ProxyNativeModule } from '@unimodules/core';
-import { NotificationRequest } from './NotificationPresenter.types';
-import { NativeNotificationTrigger } from './NotificationScheduler.types';
-import { Notification } from './NotificationsEmitter.types';
-export { NotificationRequest } from './NotificationPresenter.types';
+import { NotificationRequestInput, NotificationTriggerInput } from './NotificationScheduler.types';
+import { Notification } from './Notifications.types';
 export interface NotificationSchedulerModule extends ProxyNativeModule {
     getAllScheduledNotificationsAsync: () => Promise<Notification[]>;
-    scheduleNotificationAsync: (identifier: string, notificationRequest: NotificationRequest, trigger: NativeNotificationTrigger) => Promise<string>;
+    scheduleNotificationAsync: (identifier: string, notificationRequest: NotificationRequestInput, trigger: NotificationTriggerInput) => Promise<string>;
     cancelScheduledNotificationAsync: (identifier: string) => Promise<void>;
     cancelAllScheduledNotificationsAsync: () => Promise<void>;
 }

software-mansion-labs/expo-horizon

expo-horizon-notifications/build/NotificationScheduler.d.ts
@@ -1,11 +1,9 @@
 import { ProxyNativeModule } from '@unimodules/core';
-import { NotificationRequest } from './NotificationPresenter.types';
-import { NativeNotificationTrigger } from './NotificationScheduler.types';
-import { Notification } from './NotificationsEmitter.types';
-export { NotificationRequest } from './NotificationPresenter.types';
+import { NotificationTriggerInput } from './NotificationScheduler.types';
+import { Notification, NotificationContentInput } from './Notifications.types';
 export interface NotificationSchedulerModule extends ProxyNativeModule {
     getAllScheduledNotificationsAsync: () => Promise<Notification[]>;
-    scheduleNotificationAsync: (identifier: string, notificationRequest: NotificationRequest, trigger: NativeNotificationTrigger) => Promise<string>;
+    scheduleNotificationAsync: (identifier: string, notificationContent: NotificationContentInput, trigger: NotificationTriggerInput) => Promise<string>;
     cancelScheduledNotificationAsync: (identifier: string) => Promise<void>;
     cancelAllScheduledNotificationsAsync: () => Promise<void>;
 }