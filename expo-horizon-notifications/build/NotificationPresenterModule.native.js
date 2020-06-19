75d831139a3add43f08352826e014de7a93332df
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-06-19 02:29:04 +0200

    [expo-notifications] Add preliminary "support" for Web (#8853)
    # Why
    
    Even though we don't support Web in `expo-notifications`, we don't want to be a pain to the users and throw errors just because of running the code on Web.
    
    # How
    
    Added simple object fallbacks for missing modules on web so that they are queryable by
    ```ts
    if (!Module.methodAsync) {
      throw new Unavailability…
    }
    ```
    
    Swapped `.ts` + `.web.ts` for `.native.ts` + `.ts` (or + `.web.ts` + `.ts` where it made sense, eg. if we would need to use `localStorage` inside the `.ts` file I renamed it to `.web.ts` and added an empty fallback in `.ts`).
    
    # Test Plan
    
    - [x] Ran NCL in `bare-expo` on Web, clicked buttons around, noticed the error is readable and makes sense.
    - [x] Ran `expo-notifications` test-suite on both iOS and Web. It seems iOS tests are a little bit flaky, unfortunately (the errors could also happen due to erroring Expo API, HTTP error 504 when fetching push token, but I was able to make all of the tests pass just refreshing).

--- expo-horizon-notifications/build/NotificationPresenterModule.native.js
+++ expo-horizon-notifications/build/NotificationPresenterModule.native.js
@@ -1,3 +1,3 @@
 import { NativeModulesProxy } from '@unimodules/core';
 export default NativeModulesProxy.ExpoNotificationPresenter;
-//# sourceMappingURL=NotificationPresenter.js.map
+//# sourceMappingURL=NotificationPresenterModule.native.js.map
