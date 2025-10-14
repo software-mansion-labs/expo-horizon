2cef4ccd42e1c01b51c4517ec01e8f3fac62aecc
Bartek <116185412+benITo47@users.noreply.github.com>
2025-10-14 10:51:16 +0200

    [example] ci: pipeline integration (#16)
    * [FEAT] Add Eslint, TScheck, prettier for example app
     -Added base configurations for CI tooling
     -Fixed prettier conflicts
     -Fixed eslint issues
     -Fixed ts:check issues

--- example/app/quest.tsx
+++ example/app/quest.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
-import { Section } from "../components/Section";
-import ExpoQuest from "expo-quest";
-import { TestProperty } from "../components/TestProperty";
+import React from 'react'
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native'
+import { Section } from '../components/Section'
+import ExpoQuest from 'expo-quest'
+import { TestProperty } from '../components/TestProperty'
 
 export default function QuestScreen() {
   return (
@@ -11,20 +11,29 @@ export default function QuestScreen() {
         style={styles.scrollView}
         contentContainerStyle={styles.scrollContent}
       >
-      <Section title="Quest">
-        <TestProperty title="Is Quest Device" value={ExpoQuest.isQuestDevice.toString()} />
-        <TestProperty title="Is Quest Build" value={ExpoQuest.isQuestBuild.toString()} />
-        <TestProperty title="Quest App ID" value={ExpoQuest.questAppId?.toString() || "Not set"} />
-      </Section>
+        <Section title="Quest">
+          <TestProperty
+            title="Is Quest Device"
+            value={ExpoQuest.isQuestDevice.toString()}
+          />
+          <TestProperty
+            title="Is Quest Build"
+            value={ExpoQuest.isQuestBuild.toString()}
+          />
+          <TestProperty
+            title="Quest App ID"
+            value={ExpoQuest.questAppId?.toString() || 'Not set'}
+          />
+        </Section>
       </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
+    backgroundColor: '#f5f5f5',
   },
   scrollView: {
     flex: 1,
@@ -32,4 +41,4 @@ const styles = StyleSheet.create({
   scrollContent: {
     padding: 20,
   },
-});
+})
