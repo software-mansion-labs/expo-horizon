a2a93c197211f34989c75d63db5bccb91a7d42c6
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2025-10-20 10:41:57 +0200

    [expo-quest,example] feat: add web demo and defaults

--- expo-quest/src/ExpoQuestModule.web.ts
+++ expo-quest/src/ExpoQuestModule.web.ts
@@ -2,6 +2,10 @@ import { registerWebModule, NativeModule } from 'expo';
 
 class ExpoQuestModule extends NativeModule {
   isQuestDevice = false;
+
+  isQuestBuild = false;
+
+  questAppId = null;
 }
 
 export default registerWebModule(ExpoQuestModule, 'ExpoQuestModule');
