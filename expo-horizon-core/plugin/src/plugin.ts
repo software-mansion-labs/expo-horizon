3f1177211a8a9f2f3ce0893ee7f73fca94e7abf8
Cedric van Putten <github@cedric.dev>
2026-06-28 14:12:37 +0200

    refactor(expo-horizon-core): move types to `./withHorizon`

--- expo-horizon-core/plugin/src/plugin.ts
+++ expo-horizon-core/plugin/src/plugin.ts
@@ -1,46 +1,4 @@
-export interface ExpoHorizonCorePluginProps {
-  /**
-   * Your Meta Horizon application ID.
-   * Used by other libraries (like expo-horizon-notifications) to identify your app.
-   * @default ""
-   * @see https://developers.meta.com/horizon/resources/publish-overview-appID/#creating-an-app-id
-   */
-  horizonAppId?: string;
-
-  /**
-   * Default panel height in dp.
-   * @example "640dp"
-   * @see https://developers.meta.com/horizon/documentation/android-apps/panel-sizing
-   */
-  defaultHeight?: string;
-
-  /**
-   * Default panel width in dp.
-   * @example "1024dp"
-   * @see https://developers.meta.com/horizon/documentation/android-apps/panel-sizing
-   */
-  defaultWidth?: string;
-
-  /**
-   * Pipe-separated list of supported Quest devices.
-   * @example "quest2|quest3|quest3s"
-   * @see https://developers.meta.com/horizon/resources/publish-mobile-manifest/
-   */
-  supportedDevices: string;
-
-  /**
-   * Set to `true` to disable VR headtracking feature.
-   * By default, adds `android.hardware.vr.headtracking` to the Android Manifest.
-   */
-  disableVrHeadtracking?: boolean;
-
-  /**
-   * Set to true in the Quest build to enable Android's allowBackup feature.
-   * The default value is false which removes the "allowBackup=true" warning in the Meta Horizon Store.
-   * This does not affect your mobile build variant.
-   */
-  allowBackup?: boolean;
-}
+import type { HorizonOptions } from './withHorizon';
 
 /**
  * Config plugin that automatically configures your Android project for Meta Horizon by:
@@ -49,8 +7,6 @@ export interface ExpoHorizonCorePluginProps {
  *   - Configuring panel dimensions and supported devices
  *   - Setting up VR headtracking features
  */
-export default function expoHorizonCorePlugin(
-  props: ExpoHorizonCorePluginProps
-): [string, ExpoHorizonCorePluginProps] {
+export default function expoHorizonCorePlugin(props: HorizonOptions): [string, HorizonOptions] {
   return ['expo-horizon-core', props];
 }
